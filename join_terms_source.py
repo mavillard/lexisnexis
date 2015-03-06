2566c8011bafec9169cd1de5f0cd7b822e96e0f0
Antonio Jimenez-Mavillard <ajimene6@uwo.ca>
2015-03-06 11:41:34 -0500

    Splitting combinations into several files

--- join_terms_source.py
+++ join_terms_source.py
@@ -1,14 +1,24 @@
 import csv
+import sys
 
 
-csvfile = open('combinations.csv', 'ab')
-writer = csv.writer(
-    csvfile,
-    delimiter='\t',
-    quotechar='"',
-    quoting=csv.QUOTE_MINIMAL
-)
+num_files = sys.argv[1]
 
-for term in open('t.txt'):
-    for source in open('s.txt'):
-        writer.writerow([term.strip(), source.strip()])
+all_files = {}
+for i in range(num_files + 1):
+    filename = 'combinations_{}.csv'.format(i)
+    csvfile = open(filename, 'ab')
+    writer = csv.writer(
+        csvfile,
+        delimiter='\t',
+        quotechar='"',
+        quoting=csv.QUOTE_MINIMAL
+    )
+    all_files[i] = [filename, csvfile, writer]
+
+index = 0
+for term in open('terms.txt'):
+    for source in open('sources.txt'):
+        all_files[0][2].writerow([term.strip(), source.strip()])
+        all_files[index % num_files + 1][2].writerow([term.strip(), source.strip()])
+        index += 1
